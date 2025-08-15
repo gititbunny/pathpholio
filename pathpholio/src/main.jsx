ddf30edb2ef364841eb067166e8905e1777c5287
gititbunny <ninankhwashu@gmail.com>
2025-08-15 07:45:32 +0200

    Update main.jsx

--- pathpholio/src/main.jsx
+++ pathpholio/src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./styles.css";
+import App from "./App.jsx";
+import Dashboard from "./routes/Dashboard.jsx";
+import Auth from "./routes/Auth.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <Dashboard /> },
+      { path: "auth", element: <Auth /> },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
